b0a7ab367a849bb49fd51f69d982092adbe786eb
abbas630 <abbas@quantexai.info>
2025-12-02 17:29:16 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-
 import { Hero } from "@/components/modules/Hero";
 import { FandBSection } from "@/components/modules/FandBSection";
+import { GallerySection } from "@/components/modules/GallerySection"; // <--- Import
 import { ServicesSection } from "@/components/modules/ServicesSection";
-import { BioSection } from "@/components/modules/BioSection"; // <--- Import
+import { BioSection } from "@/components/modules/BioSection";
 import { ContactFooter } from "@/components/modules/ContactFooter";
 
 export default function Home() {
@@ -10,8 +10,9 @@ export default function Home() {
     <main className="w-full bg-background text-primary">
       <Hero />
       <FandBSection />
+      <GallerySection /> {/* <--- Inserted Here */}
       <ServicesSection />
-      <BioSection /> {/* <--- Placed between Menu and Footer */}
+      <BioSection />
       <ContactFooter />
     </main>
   );
